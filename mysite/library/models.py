63be5243b0f7c0a9c2b27dc59e6d2218fe370bd5
marijanasmolis-cmd <marijanas.molis@gmail.com>
2026-03-17 08:52:56 +0200

    2: aprasytas ir registruotas Author modelis

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
