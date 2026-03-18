f4a6aa582cb9a7ac46f9cac784a25fd7a5f7d948
marijanasmolis-cmd <marijanas.molis@gmail.com>
2026-03-18 09:30:05 +0200

    3: prie knygos dabar rodomi egzemplioriai

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
+
+class BookInstanceAdmin(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'isbn', 'display_genre')
+    inlines = [BookInstanceAdmin]
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['book', 'status', 'due_back']
+    list_filter = ['status', 'due_back']
+
+    fieldsets = [
+        ('General', {'fields': ('uuid', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    ]
 
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Book, BookAdmin)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
