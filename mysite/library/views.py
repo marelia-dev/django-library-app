e81b756a35602c1a27f83bacf34be03fdfe16610
marijanasmolis-cmd <marijanas.molis@gmail.com>
2026-04-01 09:12:42 +0300

    11: +Darbuotojui rodomas knygų egzempliorių sąrašas (meniu Administravimas)

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, reverse, redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView, FormMixin, UpdateView
@@ -127,3 +127,10 @@ def profile(request):
     }
     return render(request, template_name="profile.html", context=context)
 
+class BookInstanceListView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
+    model = BookInstance
+    template_name = "instances.html"
+    context_object_name = "instances"
+
+    def test_func(self):
+        return self.request.user.is_staff
