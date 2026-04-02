8de4c0fc0f3266efb3329a4c13fc3b5a87badde0
marijanasmolis-cmd <marijanas.molis@gmail.com>
2026-04-02 12:09:56 +0300

    12: isverstas index.html i LT

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -117,8 +117,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'lt'
+LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'lt'
 
 
 # TIME_ZONE = 'UTC'
